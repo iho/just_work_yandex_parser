75ace84e9fedf40cb33c8df079a681fc9d60d5af
Ihor Gorobets <ihorgorobets@gmail.com>
2016-08-29 19:34:49 +0300

    Add model YandexPage

--- yandex_parser/models.py
+++ yandex_parser/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class YandexPage(models.Model):
+    title = models.CharField(max_length=250)
+    url = models.TextField()
+    page = models.IntegerField(default=0)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
